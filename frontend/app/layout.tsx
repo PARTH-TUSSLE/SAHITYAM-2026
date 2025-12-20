8cf994f470bf0db85edfdf25b1aa36b2cd597971
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2025-12-20 22:54:34 +0530

    added google analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Toaster } from "react-hot-toast";
@@ -53,6 +54,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2D75SV5JMJ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2D75SV5JMJ');
+          `}
+        </Script>
+      </head>
       <body
         className={`${poppins.variable} antialiased font-sans overflow-x-hidden`}
       >
